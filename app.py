4d18e8618af6a31acfcaa75126e984b9008a44cd
Jhaendar <lycaon.anderson@gmail.com>
2022-09-15 21:06:59 +0800

    feat: added main file

--- app.py
+++ app.py
@@ -1,5 +1,23 @@
+from dotenv import load_dotenv
+import os
+import load
+
+
 def main():
-    print("Hello World!")
+    load_dotenv()
+
+    conn = get_connection_string()
+    load.load_all_tables(conn)
+
+
+def get_connection_string():
+    DB_RETAIL_USER = os.getenv("DB_RETAIL_USER")
+    DB_RETAIL_PASSWORD = os.getenv("DB_RETAIL_PASSWORD")
+    DB_HOST = os.getenv("DB_HOST")
+    DB_PORT = os.getenv("DB_PORT")
+    DB_NAME = os.getenv("DB_NAME")
+
+    return f"postgresql://{DB_RETAIL_USER}:{DB_RETAIL_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 
 if __name__ == "__main__":
